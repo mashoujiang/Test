7ff121001966fb5ba6de4360efea551645d317d0
Shoujiang Ma <shoujiang.ma@intel.com>
2021-03-31 10:22:43 +0800

    Enable google test
    Signed-off-by: Shoujiang Ma <shoujiang.ma@intel.com>

--- CppFeature.cpp
+++ CppFeature.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-class Test
+#include "gtest/gtest.h"
+
+class CppFeature: public ::testing::Test{
+
+};
+
+class Base
 {
 public:
-    Test() = default;
-    ~Test() = default;
+    Base() = default;
+    ~Base() = default;
 
 private:
     int data{0};
 };
 
-void TestPlacementNew(){
-    char buf[10000 * sizeof(Test)];
+TEST_F(CppFeature, PlacementNew){
+    char buf[10000 * sizeof(Base)];
     auto t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 10000; ++i)
     {
-        Test *test = new (buf) Test();
-        test->~Test();
+        Base *base = new (buf) Base();
+        base->~Base();
     }
     auto t2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    std::cout << "placement new: " << duration << std::endl;
+    auto placementNewDuration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
     t1 = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 10000; ++i)
     {
-        Test *test = new Test();
-        delete test;
+        Base *base = new Base();
+        delete base;
     }
     t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    std::cout << "new: " << duration << std::endl;
-}
+    auto newDuration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-int main()
-{
-    TestPlacementNew();
+    ASSERT_LT(placementNewDuration, newDuration);
 }
