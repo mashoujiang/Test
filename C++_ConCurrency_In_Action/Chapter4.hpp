afe4da9e66b0037bdf7eef53610fcec3f84c1cec
Murphy <1067294896@qq.com>
2021-06-06 21:57:21 +0800

    Exercise Chapter4 part2
    Signed-off-by: Murphy <1067294896@qq.com>

--- C++_ConCurrency_In_Action/Chapter4.hpp
+++ C++_ConCurrency_In_Action/Chapter4.hpp
@@ -55,4 +55,61 @@ private:
   mutable std::mutex _mutex;
   std::condition_variable _cond;
 };
+
+template <typename T> class my_packaged_task {};
+// 4.8 特化版std::packaged_task<>
+template <> class my_packaged_task<std::string(std::vector<char> *, int)> {
+public:
+  template <typename Callable> explicit my_packaged_task(Callable &&f);
+  std::future<std::string> get_future();
+  void operator()(std::string(std::vector<char> *, int));
+};
+
+// 4.9 std::packaged_task在GUI 多线程中的使用
+namespace _4_9 {
+bool _stop = false;
+std::deque<std::packaged_task<void()>> _tasks;
+std::mutex _mutex;
+std::condition_variable _cv;
+void GuiThread() {
+  while (!_stop) {
+    std::packaged_task<void()> task;
+    std::unique_lock<std::mutex> uniqueLock(_mutex);
+    _cv.wait(uniqueLock, []() { return !_tasks.empty(); });
+    task = std::move(_tasks.front());
+    _tasks.pop_front();
+    task();
+  }
+}
+
+template <typename Func> std::future<Func> PushTaskToGuiThread(Func f) {
+  std::packaged_task<Func> task(f);
+  auto future = task.get_future();
+  {
+    std::lock_guard<std::mutex> lockGuard(_mutex);
+    _tasks.push_back(task);
+  }
+  _cv.notify_one();
+  return future;
+}
+} // namespace _4_9
+
+// 4.11 超时等待的条件变量
+namespace _4_11 {
+std::condition_variable _cv;
+bool _done;
+std::mutex _mutex;
+bool wait_loop() {
+  auto const timeout =
+      std::chrono::steady_clock::now() + std::chrono::milliseconds(500);
+  std::unique_lock<std::mutex> uniqueLock(_mutex);
+  while (!_done) {
+    if (_cv.wait_until(uniqueLock, timeout) == std::cv_status::timeout) {
+      break;
+    }
+  }
+  return _done;
+}
+} // namespace _4_11
+
 }
